e0585ebfee7e300c7035b815fa545850047d31e1
Veronika Popova <ronipopova@gmail.com>
2021-05-12 13:48:11 +0300

    Fixed error messages

--- messages.h
+++ messages.h
@@ -14,8 +14,8 @@ const char* DAY_INVALID_ERR = "Day must be between 1 and 31!";
 const char* LEAP_FEB_DAY_INVALID_ERR = "On leap years february has 29 days!";
 const char* FEB_DAY_INVALID_ERR = "February has only 28 days!";
 const char* DAYS30_INVALID_ERR = "That month has only 30 days!";
-const char* MONTH_IVALID_ERR = "Month has to be between 1 and 12";
-const char* YEAR_INVALID_ERR = "Year must be a positive number";
+const char* MONTH_IVALID_ERR = "Month has to be between 1 and 12!";
+const char* YEAR_INVALID_ERR = "Year must be a positive number!";
 
 const char* PHONE_CONTAINS_NONDIGIT_ERR = "Phone number must be only digits!";
 const char* PHONE_LENGTH_ERR = "Phone number must be 10 digits!";
